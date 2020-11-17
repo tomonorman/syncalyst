2624844e74a465c1ac864cd82dbba42ad7b3d78f
Takuhei Mizutani <takuheim1011@gmail.com>
2020-11-17 14:16:36 +0900

    added styling and changed bootstrap variable for border-radius

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,6 +1,27 @@
 class MeetingsController < ApplicationController
+  before_action :set_meeting, only: :create
+  def index
+    @meetings = policy_scope(current_user.meetings)
+  end
 
   def new
+    @meeting = Meeting.new
+    authorize @meeting
+  end
 
+  def create
+    @meeting.user = current_user
+    if @meeting.save
+      redirect_to meetings_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def set_meeting
+    @meeting = Meeting.new(params[:id])
+    authorize @meeting
   end
 end
--- app/policies/meeting_policy.rb
+++ app/policies/meeting_policy.rb
@@ -4,4 +4,12 @@ class MeetingPolicy < ApplicationPolicy
       scope.all
     end
   end
+
+  def new?
+    true
+  end
+
+  def create?
+    user
+  end
 end
