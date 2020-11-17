5d65bd92744ef04df7b7e62e97adf4cfc3c4d254
Takuhei Mizutani <takuheim1011@gmail.com>
2020-11-17 13:18:58 +0900

    added routing & controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :meetings, only: [:index, :show, :edit] do
+    resources :attendances, only: [:create]
+    resources :agendas, only: [:create]
+    resources :tasks, only: [:create]
+  end
+  resources :attendances, only: [:edit]
+  resources :agendas, only: [:edit, :start, :finish]
 end
