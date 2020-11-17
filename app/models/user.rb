6f6d2cb444c851d31c790e43adbb43548dff5882
Tomo Norman <tomo.l.norman@gmail.com>
2020-11-17 11:54:21 +0900

    fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
     :recoverable, :rememberable, :validatable
   has_one_attached :photo
-  has_many :tasks, :attendances
+  has_many :tasks
+  has_many :attendances
+  has_many :meetings, through: :attendances
+  has_many :attendances_as_owner, through: :meetings, source: :attendances
 end
