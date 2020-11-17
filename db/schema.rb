fc2f37802135df5445cab233202462a5317c3706
Takuhei Mizutani <takuheim1011@gmail.com>
2020-11-17 10:58:13 +0900

    added attendance model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_16_090359) do
+ActiveRecord::Schema.define(version: 2020_11_17_014920) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,16 @@ ActiveRecord::Schema.define(version: 2020_11_16_090359) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "attendances", force: :cascade do |t|
+    t.boolean "status", default: false
+    t.bigint "user_id", null: false
+    t.bigint "meeting_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["meeting_id"], name: "index_attendances_on_meeting_id"
+    t.index ["user_id"], name: "index_attendances_on_user_id"
   end
 
   create_table "meetings", force: :cascade do |t|
@@ -63,5 +73,7 @@ ActiveRecord::Schema.define(version: 2020_11_16_090359) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "attendances", "meetings"
+  add_foreign_key "attendances", "users"
   add_foreign_key "meetings", "users"
 end
