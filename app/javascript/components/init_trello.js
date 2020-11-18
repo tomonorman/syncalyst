da0d0432d2241836896bc1e474bae60a9f465aec
Jessica Ho <hwyann@gmail.com>
2020-11-18 19:05:06 +0900

    added some trello stuff

---
+++ app/javascript/components/init_trello.js
@@ -0,0 +1,22 @@
+const initTrello = () => {
+  const authenticationSuccess = () => {
+    console.log('Successful authentication');
+  };
+
+  const authenticationFailure = () => {
+    console.log('Failed authentication');
+  };
+
+  window.Trello.authorize({
+    type: 'popup',
+    name: 'Getting Started Application',
+    scope: {
+      read: 'true',
+      write: 'true' },
+    expiration: 'never',
+    success: authenticationSuccess,
+    error: authenticationFailure
+  });
+}
+
+export { initTrello }
