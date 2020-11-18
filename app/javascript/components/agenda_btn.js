84a056138c3f7cf106d092ea77547eae6550ecbc
Jessica Ho <hwyann@gmail.com>
2020-11-18 14:04:35 +0900

    agenda form created on meeting

--- app/javascript/components/agenda_btn.js
+++ app/javascript/components/agenda_btn.js
@@ -1,7 +1,7 @@
 const initAgendaBtn = () => {
-const agendaForm = document.querySelector(".agenda-form");
-const addAgendaBtn = document.querySelector(".add-agenda-btn");
-const agendaInput = document.querySelector(".agenda-input");
+  const agendaForm = document.querySelector(".agenda-form");
+  const addAgendaBtn = document.querySelector(".add-agenda-btn");
+  const agendaInput = document.querySelector(".agenda-input");
 
 addAgendaBtn.addEventListener("click", event => {
     event.preventDefault();
@@ -11,4 +11,20 @@ addAgendaBtn.addEventListener("click", event => {
 
 };
 
+// const initAgendaColl = () => {
+//   const coll = document.getElementsByClassName("collapsible");
+//   const i;
+
+//   coll.addEventListener("click", (event) {
+//       this.classList.toggle("active");
+//       const content = this.nextElementSibling;
+//       if (content.style.display === "block") {
+//         content.style.display = "none";
+//       } else {
+//         content.style.display = "block";
+//       }
+//     });
+// };
+
 export { initAgendaBtn };
+// export { initAgendaColl };
