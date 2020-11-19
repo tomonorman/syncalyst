80df49797c66036cc389e80f67db534f45a51a18
Jessica Ho <hwyann@gmail.com>
2020-11-19 13:27:25 +0900

    added event listener to task creation

--- app/javascript/components/init_card.js
+++ app/javascript/components/init_card.js
@@ -1,7 +1,9 @@
-// https://trello.com/b/hmiXsuho/syncalyst.json
+// get idlist from https://trello.com/b/hmiXsuho/syncalyst.json
+
 const initCard = () => {
   const myList = '5fb4e5f006c65105637a41a6';
   const taskCreation = document.querySelector("#new_task");
+  const newTaskDesc = document.querySelector('#task_description').value;
 
   const creationSuccess = function (data) {
     console.log('Card created successfully.');
@@ -10,7 +12,7 @@ const initCard = () => {
 
   const newCard = {
     name: 'New Test Card',
-    desc: 'This is the description of our new card.',
+    desc: newTaskDesc,
     // Place this card at the top of our list
     idList: myList,
     pos: 'top'
