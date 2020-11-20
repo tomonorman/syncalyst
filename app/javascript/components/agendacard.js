7253a2ee4a5c762e517ea3b5b6ade8864c881371
Tomo Norman <tomo.l.norman@gmail.com>
2020-11-20 14:24:57 +0900

    script added to js to show transcript

--- app/javascript/components/agendacard.js
+++ app/javascript/components/agendacard.js
@@ -2,9 +2,28 @@ const transcription = (index) => {
   const transcriptionItems = document.querySelectorAll(".transcription");
   transcriptionItems.forEach((transcription) => {
     transcription.classList.add("transcription-display");
+    transcription.classList.remove("typewriter");
   });
   transcriptionItems[index].classList.remove("transcription-display");
+  transcriptionItems[index].classList.add("typewriter");
+  setTimeout(function(){
+    typeWriter()
+  }, 5000);
+  n++;
 };
+
+let n = -1;
+let i = 0;
+const txt = ["Hi Jess. This will follow you while you are talking and this should be on a new line", "This will appear on the second agenda", "This will appear on the third agenda", "this will appear on the 4th agenda"];
+const speed = 100;
+
+const typeWriter = () => {
+  if (i < txt[n].length) {
+    document.querySelector(".typewriter").innerHTML += txt[n].charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
 
 const initAgenda = () => {
   const agendaItems = document.querySelectorAll(".agenda-cards-inprogress");
@@ -20,6 +39,7 @@ const initAgenda = () => {
         event.currentTarget.classList.add("active");
         const array = Array.from(agendaItems);
         const index = array.indexOf(event.currentTarget);
+        i = 0;
         transcription(index);
       });
     });
