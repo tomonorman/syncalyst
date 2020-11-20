a9ca671aa2ef0944ef5593c4b7ae1ce9cbe8f65a
Tomo Norman <tomo.l.norman@gmail.com>
2020-11-20 10:48:49 +0900

    javascript fixed

--- app/javascript/components/agendacard.js
+++ app/javascript/components/agendacard.js
@@ -9,19 +9,21 @@ const transcription = (index) => {
 const initAgenda = () => {
   const agendaItems = document.querySelectorAll(".agenda-cards-inprogress");
 
-  agendaItems.forEach((item) => {
-    item.addEventListener('click', (event) => {
-      agendaItems.forEach((card) => {
-        if (card.classList.contains("active")) {
-          card.classList.remove("active");
-        }
+  if (agendaItems) {
+    agendaItems.forEach((item) => {
+      item.addEventListener('click', (event) => {
+        agendaItems.forEach((card) => {
+          if (card.classList.contains("active")) {
+            card.classList.remove("active");
+          }
+        });
+        event.currentTarget.classList.add("active");
+        const array = Array.from(agendaItems);
+        const index = array.indexOf(event.currentTarget);
+        transcription(index);
       });
-      event.currentTarget.classList.add("active");
-      const array = Array.from(agendaItems);
-      const index = array.indexOf(event.currentTarget);
-      transcription(index);
     });
-  });
+  }
 }
 
 export { initAgenda }
