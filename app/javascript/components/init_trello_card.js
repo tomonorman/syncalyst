1f248ae589f2b5d7188df63c1a23e87ad6a24700
Jessica Ho <hwyann@gmail.com>
2020-11-20 14:25:13 +0900

    user added when create a trello card

--- app/javascript/components/init_trello_card.js
+++ app/javascript/components/init_trello_card.js
@@ -13,12 +13,16 @@ if (taskForm) {
   taskForm.forEach((form) => {
      form.addEventListener("submit", event => {
       const newTaskDesc = form.querySelector('.task_description input');
-      console.log(newTaskDesc.value);
+      const userInput = form.querySelector('.task_user_id input');
+      const user = userInput.dataset.trelloid;
+      // gets the user instance of the task
+
       const newCard = {
         name: newTaskDesc.value,
-        desc: 'new desc',
-        // Place this card at the top of our list
+        desc: 'task assigned from Syncalyst meeting',
+        idMembers: [user],
         idList: myList,
+        // Place this card at the top/bottom of our list
         pos: 'top'
       };
       window.Trello.post('/cards/', newCard, creationSuccess);
