edeb86f5787026d49d7c5230d74b8ff69531d2c9
Tomo Norman <tomo.l.norman@gmail.com>
2020-11-22 16:39:36 +0900

    speech to text works on the agenda page

--- app/javascript/components/agendacard.js
+++ app/javascript/components/agendacard.js
@@ -2,28 +2,58 @@ const transcription = (index) => {
   const transcriptionItems = document.querySelectorAll(".transcription");
   transcriptionItems.forEach((transcription) => {
     transcription.classList.add("transcription-display");
-    transcription.classList.remove("typewriter");
+    transcription.innerHTML = "";
   });
   transcriptionItems[index].classList.remove("transcription-display");
-  transcriptionItems[index].classList.add("typewriter");
-  setTimeout(function(){
-    typeWriter()
-  }, 3000);
-  n++;
+  transcriptionItems[index].insertAdjacentHTML("beforeend", "<div class='form-group'><textarea class='form-control' id='textbox' rows='10'></textarea></div><div class='form-group'><button id='start-btn' class='btn btn-primary btn-block'>start</button><button id='stop-btn' class='btn btn-primary btn-block'>stop</button><p id='instructions'>Press start to record</p></div>")
 };
 
-let n = -1;
-let i = 0;
-const txt = ["Jess: it should start recording and transcribing automatically. Last week I have made a new button on the agenda page. Let's go to the next item on the agenda", "Jess: But we have one problem. That its loading the page very slowly so we need to fix it.", "Jess: Let's talk about other new ideas. How about adding a search function? How about you Tomo?", "Tomo: We need to add more animations."];
-const speed = 80;
+const initSpeech = () => {
+  var speechRecognition = window.webkitSpeechRecognition;
+  let recognition = new speechRecognition();
+  var textbox = $("#textbox");
+  let instructions = $("#instructions");
+  let content = '';
 
-const typeWriter = () => {
-  if (i < txt[n].length) {
-    document.querySelector(".typewriter").innerHTML += txt[n].charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
+  recognition.continuous = true;
+
+  recognition.onstart = function() {
+    instructions.text("Voice Recognition is on");
   }
+
+  recognition.onspeechend = function() {
+    instructions.text("No Activity");
+  }
+
+  recognition.onerror = function() {
+    instructions.text("Try again");
+  }
+
+  recognition.onresult = function (event) {
+    var  current = event.resultIndex;
+    var transcript = event.results[current][0].transcript;
+    content += transcript;
+    textbox.val(content);
+  }
+
+  $("#start-btn").click(function (event) {
+    console.log('hello');
+    if(content.length) {
+      content += '';
+    }
+    recognition.start();
+  })
+
+  $("#stop-btn").click(function (event) {
+    recognition.stop();
+    content = '';
+  })
+
+  textbox.on('input', function () {
+    content = $(this).val();
+  })
 }
+
 
 const initAgenda = () => {
   const agendaItems = document.querySelectorAll(".agenda-cards-inprogress");
@@ -39,8 +69,8 @@ const initAgenda = () => {
         event.currentTarget.classList.add("active");
         const array = Array.from(agendaItems);
         const index = array.indexOf(event.currentTarget);
-        i = 0;
         transcription(index);
+        initSpeech();
       });
     });
   }
